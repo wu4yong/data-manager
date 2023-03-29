04d74d72da19ab8d3248d76bcc840275a9d07a5d
wuyong <wu4yong@163.com>
2023-03-29 11:23:56 +0800

    init project

---
+++ src/main/java/com/qili/datamanager/DataManagerApplication.java
@@ -0,0 +1,14 @@
+package com.qili.datamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class DataManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qili/datamanager/DataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qili.datamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
