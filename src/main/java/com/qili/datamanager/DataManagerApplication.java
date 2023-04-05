774ca3bae0ee0f879da08ececf63dd4300a44aa2
wuyong <wu4yong@163.com>
2023-04-05 10:58:30 +0800

    修改配置

--- src/main/java/com/qili/datamanager/DataManagerApplication.java
+++ src/main/java/com/qili/datamanager/DataManagerApplication.java
@@ -2,8 +2,9 @@ package com.qili.datamanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DataManagerApplication {
 
--- src/main/java/com/qili/datamanager/vo/HbaseTableInfo.java
+++ src/main/java/com/qili/datamanager/vo/HbaseTableInfo.java
@@ -1,8 +1,6 @@
 package com.qili.datamanager.vo;
 
-
-import jakarta.validation.constraints.NotBlank;
-
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 /**
